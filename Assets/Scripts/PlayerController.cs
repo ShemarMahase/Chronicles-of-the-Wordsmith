59500dc61ab67c6363f2ae31f1f627c517ec1c4e
Criv21 <acrivello21@gmail.com>
2025-06-25 00:14:21 -0400

    added animation, collision, and started on interactions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,7 +11,16 @@ public class PlayerController : MonoBehaviour
     private bool isMoving;
 
     private Vector2 input;
-    
+
+    public LayerMask solidObjectsLayer;
+    public LayerMask interactableLayer;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     private void Update()
     {
         if (!isMoving)
@@ -17,21 +28,39 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+            //prevent player from moving diagonally
             if (input.x != 0) input.y = 0;
 
+            //begin movement
             if (input != Vector2.zero)
             {
+                //set directions for animation
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
-                StartCoroutine(Move(targetPos));
+                if (isWalkable(targetPos))
+                {
+                    StartCoroutine(Move(targetPos));
+                }
             }
+        }
+
+        animator.SetBool("isMoving", isMoving);
+
+        //interact input
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Interact();
         }
     }
 
     IEnumerator Move(Vector3 targetPos)
     {
+        //move player one space in a direction based on set moveSpeed
         isMoving = true;
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
@@ -41,5 +70,32 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+
+    private bool isWalkable(Vector3 targetPos)
+    {
+        //check if the bottom half of the player will collide next move
+        Vector3 checkPos = targetPos + new Vector3(0, -0.5f, 0);
+        if (Physics2D.OverlapCircle(checkPos, 0.2f, solidObjectsLayer | interactableLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    void Interact()
+    {
+        //get position in front of player
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var InteractPos = transform.position + facingDir;
+
+        var collider = Physics2D.OverlapCircle(InteractPos, 0.2f, interactableLayer);
+        
+        //do interaction stuff idk
+        if (collider != null)
+        {
+
+        }
     }
 }
